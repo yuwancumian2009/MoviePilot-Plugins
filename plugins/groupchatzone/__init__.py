1367030de3825674c019f3811c7ff7950a07522f
KoWming <96684377+KoWming@users.noreply.github.com>
2025-03-11 20:07:16 +0800

    Update __init__.py

--- plugins/groupchatzone/__init__.py
+++ plugins/groupchatzone/__init__.py
@@ -34,7 +34,7 @@ class GroupChatZone(_PluginBase):
     # 插件版本
     plugin_version = "1.3.0"
     # 插件作者
-    plugin_author = "KoWming"
+    plugin_author = "KoWming,madrays"
     # 作者主页
     author_url = "https://github.com/KoWming"
     # 插件配置项ID前缀
